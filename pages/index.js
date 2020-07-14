dee7892aac420a95bf5be4a1bbc730e7705c9619
unknown <gabrielcmo2018@gmail.com>
2020-07-14 18:37:10 -0300

    Add service static

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 const Home = () => (
     <div>
+        <Head>
+            <title>Hello World</title>
+        </Head>
+
+        <img src="static/panda.jpg" width="200"/>
+        
         <h1>Hello World!</h1>
         <Link href="/users">
             <a>Usuários</a>

0e6d7d5be88da1f5097121dc3cd3b964f5d0a307
unknown <gabrielcmo2018@gmail.com>
2020-07-14 18:38:19 -0300

    overwriting  this document in next

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render()
+    {
+        return(
+            <html>
+                <Head>
+                    
+                </Head>
+
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}
